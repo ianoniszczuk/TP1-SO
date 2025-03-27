876b361529c70d29d4104d7a28984138de1fb564
Ian Cruz Oniszczuk <152518348+ianoniszczuk@users.noreply.github.com>
2025-03-27 17:19:02 -0300

    pruebas

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
-gcc -Wall -pedantic -std=c99 ./view/view.c -o executables/view
-gcc -Wall -pedantic -std=c99 ./player/player.c -o executables/player
-gcc -Wall -pedantic -std=c99 ./master/master.c -o executables/master
+gcc -Wall -pedantic -std=c99 ./view/view.c -o executables/view.o
+gcc -Wall -pedantic -std=c99 ./player/player.c -o executables/player.o
+gcc -Wall -pedantic -std=c99 ./master/master.c -o executables/master.o
