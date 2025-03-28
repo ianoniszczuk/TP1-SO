c44a3f38dbb1f6010bc8b678f7aed7c16785afb2
jicanta <ignaciocantarella@gmail.com>
2025-03-28 10:57:53 -0300

    remove unnecesary header

--- errorHandling.h
+++ errorHandling.h
@@ -2,7 +2,7 @@
 #define ERROR_HANDLING_H
 
 #define ERROR_EXIT(msg)     do { perror(msg); exit(EXIT_FAILURE); } while (0)
-// TODO: veri si lo necesitamos
+// TODO: ver si lo necesitamos
 // #define CHECK_NULL(x)       if ((x) == NULL) ERROR_EXIT("malloc")
 
 #endif
